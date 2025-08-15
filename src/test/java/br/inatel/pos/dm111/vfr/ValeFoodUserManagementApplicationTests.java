af89b345a692155f65faf5240f514acf1de23d3e
joseefrodriguesbr <jose.e.f.rodrigues.br@gmail.com>
2025-08-14 21:14:57 -0300

    Initial commit

---
+++ src/test/java/br/inatel/pos/dm111/vfr/ValeFoodUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.pos.dm111.vfr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValeFoodUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
